61191dd988593e97b032cfd784cdbbb0922b020d
Gautamyadav824 <gautamahir20005@gmail.com>
2026-06-27 12:59:02 +0530

    fix

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import HomePage from './landing_page/home/HomePage';
 // import Signup from "./landing_page/signup/Signup";
 import AboutPage from "./landing_page/about/AboutPage";
-// import ProductPage from "./landing_page/products/ProductPage";
+import ProductPage from "./landing_page/products/ProductPage";
 // import PricingPage from "./landing_page/pricing/PricingPage";
 // import SupportPage from "./landing_page/support/SupportPage";
 // import Navbar from './landing_page/Navbar';
@@ -29,6 +29,7 @@ root.render(
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<AboutPage />} />
+      <Route path="/products" element={<ProductPage />} />
     </Routes>
 
     <Footer />
