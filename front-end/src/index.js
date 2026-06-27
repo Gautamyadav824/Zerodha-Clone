7319dc0893307adcd8dfbf97459e3c71af83cbed
Gautamyadav824 <gautamahir20005@gmail.com>
2026-06-27 12:51:23 +0530

    fix

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+
 import Navbar from "./landing_page/Navbar";
-// import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 // import './index.css';
 import HomePage from './landing_page/home/HomePage';
 // import Signup from "./landing_page/signup/Signup";
