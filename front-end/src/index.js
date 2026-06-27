b23332c1caa20df529523a06270f40e6edd9494f
Gautamyadav824 <gautamahir20005@gmail.com>
2026-06-27 13:04:08 +0530

    fix

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -8,8 +8,8 @@ import HomePage from './landing_page/home/HomePage';
 // import Signup from "./landing_page/signup/Signup";
 import AboutPage from "./landing_page/about/AboutPage";
 // import ProductPage from "./landing_page/products/ProductPage";
-import PricingPage from "./landing_page/pricing/PricingPage";
-// import SupportPage from "./landing_page/support/SupportPage";
+// import PricingPage from "./landing_page/pricing/PricingPage";
+import SupportPage from "./landing_page/support/SupportPage";
 // import Navbar from './landing_page/Navbar';
 import Footer from './landing_page/Footer';
 // import NotFound from './landing_page/NotFound';
@@ -30,7 +30,8 @@ root.render(
       <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<AboutPage />} />
       {/* <Route path="/products" element={<ProductPage />} /> */}
-      <Route path="/pricing" element={<PricingPage />} />
+      {/* <Route path="/pricing" element={<PricingPage />} /> */}
+      <Route path="/support" element={<SupportPage />} />
     </Routes>
 
     <Footer />
