ea6b77e96db5f8baf32afe4e3fcfdd76443f384c
Gautamyadav824 <gautamahir20005@gmail.com>
2026-06-27 13:28:22 +0530

    find error

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -5,7 +5,7 @@ import Navbar from "./landing_page/Navbar";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 // import './index.css';
 import HomePage from './landing_page/home/HomePage';
-// import Signup from "./landing_page/signup/Signup";
+import Signup from "./landing_page/signup/Signup";
 import AboutPage from "./landing_page/about/AboutPage";
 // import ProductPage from "./landing_page/products/ProductPage";
 // import PricingPage from "./landing_page/pricing/PricingPage";
